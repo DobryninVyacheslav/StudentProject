ebea8ce8b2384fefdbd6bbc4cbab722c4a3eb6f0
SlavaDobr <slavad999@gmail.com>
2020-07-31 21:41:13 +0300

    refactoring of db connection

--- src/main/java/edu/javacourse/studentorder/dao/DictionaryDaoImpl.java
+++ src/main/java/edu/javacourse/studentorder/dao/DictionaryDaoImpl.java
@@ -22,6 +22,7 @@ public class DictionaryDaoImpl implements DictionaryDao {
     private static final String GET_AREA = "SELECT * " +
             "FROM jc_country_struct WHERE area_id LIKE ? AND area_id <> ?";
 
+    //TODO refactoring - make one method
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
                 Config.getProperty(Config.DB_URL),
