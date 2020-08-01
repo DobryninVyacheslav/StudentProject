a7c1132b8fe9188be10fcab4695e1a3495fd66cb
SlavaDobr <slavad999@gmail.com>
2020-08-01 12:33:06 +0300

    refactoring of db connection

--- src/main/java/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/main/java/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -1,6 +1,8 @@
 package edu.javacourse.studentorder;
 
 import edu.javacourse.studentorder.dao.DictionaryDaoImpl;
+import edu.javacourse.studentorder.dao.StudentOrderDao;
+import edu.javacourse.studentorder.dao.StudentOrderDaoImpl;
 import edu.javacourse.studentorder.domain.*;
 
 import java.sql.*;
@@ -10,39 +12,44 @@ import java.util.List;
 public class SaveStudentOrder
 {
     public static void main(String[] args) throws Exception {
-        List<Street> d = new DictionaryDaoImpl().findStreet("про");
-        for (Street street : d) {
-            System.out.println(street.getStreetName());
+//        List<Street> d = new DictionaryDaoImpl().findStreet("про");
+//        for (Street street : d) {
+//            System.out.println(street.getStreetName());
+//        }
+//        List<PassportOffice> po = new DictionaryDaoImpl().findPassportOffices("010020000000");
+//        for (PassportOffice p : po) {
+//            System.out.println(p.getOfficeName());
+//        }
+//        List<RegisterOffice> ro = new DictionaryDaoImpl().findRegisterOffices("010010000000");
+//        for (RegisterOffice r : ro) {
+//            System.out.println(r.getOfficeName());
+//        }
+//
+//        List<CountryArea> ca1 = new DictionaryDaoImpl().findAreas("");
+//        for (CountryArea ca : ca1) {
+//            System.out.println(ca.getAreaId() + ":" +ca.getAreaName());
+//        }
+//        System.out.println("--->");
+//        List<CountryArea> ca2 = new DictionaryDaoImpl().findAreas("020000000000");
+//        for (CountryArea ca : ca2) {
+//            System.out.println(ca.getAreaId() + ":" +ca.getAreaName());
+//        }
+//        System.out.println("--->");
+//        List<CountryArea> ca3 = new DictionaryDaoImpl().findAreas("020010000000");
+//        for (CountryArea ca : ca3) {
+//            System.out.println(ca.getAreaId() + ":" +ca.getAreaName());
+//        }
+//        System.out.println("--->");
+//        List<CountryArea> ca4 = new DictionaryDaoImpl().findAreas("020010010000");
+//        for (CountryArea ca : ca4) {
+//            System.out.println(ca.getAreaId() + ":" +ca.getAreaName());
+//        }
+        StudentOrder s = buildStudentOrder(10);
+        StudentOrderDao dao = new StudentOrderDaoImpl();
+        for (int i = 0; i < 5; i++) {
+            Long id = dao.saveStudentOrder(s);
+            System.out.println(id);
         }
-        List<PassportOffice> po = new DictionaryDaoImpl().findPassportOffices("010020000000");
-        for (PassportOffice p : po) {
-            System.out.println(p.getOfficeName());
-        }
-        List<RegisterOffice> ro = new DictionaryDaoImpl().findRegisterOffices("010010000000");
-        for (RegisterOffice r : ro) {
-            System.out.println(r.getOfficeName());
-        }
-
-        List<CountryArea> ca1 = new DictionaryDaoImpl().findAreas("");
-        for (CountryArea ca : ca1) {
-            System.out.println(ca.getAreaId() + ":" +ca.getAreaName());
-        }
-        System.out.println("--->");
-        List<CountryArea> ca2 = new DictionaryDaoImpl().findAreas("020000000000");
-        for (CountryArea ca : ca2) {
-            System.out.println(ca.getAreaId() + ":" +ca.getAreaName());
-        }
-        System.out.println("--->");
-        List<CountryArea> ca3 = new DictionaryDaoImpl().findAreas("020010000000");
-        for (CountryArea ca : ca3) {
-            System.out.println(ca.getAreaId() + ":" +ca.getAreaName());
-        }
-        System.out.println("--->");
-        List<CountryArea> ca4 = new DictionaryDaoImpl().findAreas("020010010000");
-        for (CountryArea ca : ca4) {
-            System.out.println(ca.getAreaId() + ":" +ca.getAreaName());
-        }
-//        StudentOrder s = buildStudentOrder(10);
 //        StudentOrder so = new StudentOrder();
 //        long ans = saveStudentOrder(so);
 //        System.out.println(ans);
