c8761081600914192b302aba5d8135539ad2a66c
SlavaDobr <slavad999@gmail.com>
2020-08-25 22:02:40 +0300

    added sql query

--- ExtSystems/CityRegister/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ ExtSystems/CityRegister/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -11,7 +11,19 @@ import java.sql.SQLException;
 
 public class PersonCheckDao {
 
-    private static final String SQL_REQUEST = "";
+    private static final String SQL_REQUEST =
+            "select temporal, upper(p.sur_name), upper('Васильев') from cr_address_person ap " +
+            "inner join cr_person p on p.person_id = ap.person_id " +
+            "inner join cr_address a on a.address_id = ap.address_id " +
+            "where " +
+            "upper(p.sur_name) = upper(?) " +
+            "and upper(p.given_name) = upper(?) " +
+            "and upper(p.patronymic) = upper(?) " +
+            "and p.date_of_birth = ? " +
+            "and a.street_code = ? " +
+            "and upper(a.building) = upper(?) " +
+            "and upper(extension) = upper(?) " +
+            "and upper(a.apartment) = upper(?)";
 
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
         PersonResponse response = new PersonResponse();
