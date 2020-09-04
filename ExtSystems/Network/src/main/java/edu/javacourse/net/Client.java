f990e4dd5db57eac813203a55addc5d3e493eff9
SlavaDobr <slavad999@gmail.com>
2020-09-04 20:29:58 +0300

    added commands

--- ExtSystems/Network/src/main/java/edu/javacourse/net/Client.java
+++ ExtSystems/Network/src/main/java/edu/javacourse/net/Client.java
@@ -6,28 +6,39 @@ import java.time.LocalDateTime;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 1000; i++) {
-            SimpleClient client = new SimpleClient();
+        for (int i = 0; i < 8; i++) {
+            SimpleClient client = new SimpleClient(i);
             client.start();
         }
     }
 }
 
 class SimpleClient extends Thread {
+
+    private static final String[] COMMAND = {
+            "HELLO", "MORNING", "DAY", "EVENING"
+    };
+    private int cmdNumber;
+
+    public SimpleClient(int cmdNumber) {
+        this.cmdNumber = cmdNumber;
+    }
+
     @Override
     public void run() {
-        System.out.println("Started:" + LocalDateTime.now());
+//        System.out.println("Started:" + LocalDateTime.now());
         try (Socket client = new Socket("127.0.0.1", 25225);
              BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()))) {
-            String builder = "Slava";
+            String command = COMMAND[cmdNumber % COMMAND.length];
+            String builder = command + " " + "Slava";
             writer.write(builder);
             writer.newLine();
             writer.flush();
 
             String answer = reader.readLine();
             System.out.println("Client got string:" + answer);
-            System.out.println("Finished:" + LocalDateTime.now());
+//            System.out.println("Finished:" + LocalDateTime.now());
         } catch (IOException e) {
             e.printStackTrace();
         }
--- ExtSystems/Network/src/main/java/edu/javacourse/net/Server.java
+++ ExtSystems/Network/src/main/java/edu/javacourse/net/Server.java
@@ -3,9 +3,10 @@ package edu.javacourse.net;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Path;
 
 public class Server {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket socket = new ServerSocket(25225, 2000)) {
             System.out.println("Server is started");
             while (true) {
@@ -32,15 +33,33 @@ class SimpleServer extends Thread {
     private void handleRequest() {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()))) {
-            StringBuilder builder = new StringBuilder("Hello, ");
-            String userName = reader.readLine();
-            System.out.println("Server got string:" + userName);
-            Thread.sleep(2000);
-            builder.append(userName);
-            writer.write(builder.toString());
+            String request = reader.readLine();
+            String[] lines = request.split("\\s+");
+            String command = lines[0];
+            String userName = lines[1];
+            System.out.println("Server got string 1:" + command);
+            System.out.println("Server got string 2:" + userName);
+//            Thread.sleep(2000);
+            String response = buildResponse(command, userName);
+            writer.write(response);
             writer.newLine();
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private String buildResponse(String command, String userName) {
+        switch (command) {
+            case "HELLO":
+                return "Hello, " + userName;
+            case "MORNING":
+                return "Good morning, " + userName;
+            case "DAY":
+                return "Good day, " + userName;
+            case "EVENING":
+                return "Good evening, " + userName;
+            default:
+                return "Hi, " + userName;
         }
     }
 }
