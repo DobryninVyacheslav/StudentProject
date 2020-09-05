5f3532029bf0cbd9b8be8d5f948d56b282e350df
SlavaDobr <slavad999@gmail.com>
2020-09-05 14:34:52 +0300

    testing localhost

--- ExtSystems/CityRegister/src/test/java/edu/javacourse/net/SimpleSocket.java
+++ ExtSystems/CityRegister/src/test/java/edu/javacourse/net/SimpleSocket.java
@@ -11,10 +11,10 @@ public class SimpleSocket {
 
     @Test
     public void simpleSocket() throws IOException {
-        try (Socket socket = new Socket("java-course.ru", 80);
+        try (Socket socket = new Socket("localhost", 8080);
              InputStream is = socket.getInputStream();
              OutputStream os = socket.getOutputStream()) {
-            String command = "GET /sitemap.xml HTTP/1.1\r\nHost:java-course.ru\r\n\r\n";
+            String command = "GET / HTTP/1.1\r\nHost:java-course.ru\r\n\r\n";
             os.write(command.getBytes());
             os.flush();
 
