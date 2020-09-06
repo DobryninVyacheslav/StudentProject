2c53ab571a42915bef01abc2aa3a383d899e161b
SlavaDobr <slavad999@gmail.com>
2020-09-06 22:00:34 +0300

    ReST with db

--- ExtSystems/CityRegister/src/main/java/edu/javacourse/city/web/CheckPersonService.java
+++ ExtSystems/CityRegister/src/main/java/edu/javacourse/city/web/CheckPersonService.java
@@ -1,8 +1,15 @@
 package edu.javacourse.city.web;
 
+import edu.javacourse.city.dao.PersonCheckDao;
+import edu.javacourse.city.dao.PoolConnectionBuilder;
 import edu.javacourse.city.domian.PersonRequest;
 import edu.javacourse.city.domian.PersonResponse;
+import edu.javacourse.city.exception.PersonCheckException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,13 +17,24 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/check")
+@Singleton
 public class CheckPersonService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CheckPersonService.class);
+    private PersonCheckDao dao;
+
+    @PostConstruct
+    public void init() {
+        LOG.info("SERVICE is created");
+        dao = new PersonCheckDao();
+        dao.setConnectionBuilder(new PoolConnectionBuilder());
+    }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public PersonResponse checkPerson(PersonRequest request) {
-        System.out.println(request.toString());
-        return new PersonResponse();
+    public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
+        LOG.info(request.toString());
+        return dao.checkPerson(request);
     }
 }
