6f3231d82faa943a3ba292d16e7eac6150bfc736
SlavaDobr <slavad999@gmail.com>
2020-09-06 21:18:34 +0300

    added adapter for date

--- ExtSystems/CityRegister/src/main/java/edu/javacourse/city/web/CheckPersonService.java
+++ ExtSystems/CityRegister/src/main/java/edu/javacourse/city/web/CheckPersonService.java
@@ -1,17 +1,22 @@
 package edu.javacourse.city.web;
 
+import edu.javacourse.city.domian.PersonRequest;
 import edu.javacourse.city.domian.PersonResponse;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/check")
 public class CheckPersonService {
-    @GET
-    @Path("/{id}")
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public PersonResponse checkPerson(@PathParam("id") int id,
-                                      @QueryParam("name") String name) {
+    public PersonResponse checkPerson(PersonRequest request) {
+        System.out.println(request.toString());
         return new PersonResponse();
     }
 }
