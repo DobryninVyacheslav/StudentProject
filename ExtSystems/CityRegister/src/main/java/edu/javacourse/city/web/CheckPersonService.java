c15a5d18b51a41a243de01193e10620a7867471f
SlavaDobr <slavad999@gmail.com>
2020-09-06 20:16:21 +0300

    in and out params in ReST

--- ExtSystems/CityRegister/src/main/java/edu/javacourse/city/web/CheckPersonService.java
+++ ExtSystems/CityRegister/src/main/java/edu/javacourse/city/web/CheckPersonService.java
@@ -1,12 +1,17 @@
 package edu.javacourse.city.web;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import edu.javacourse.city.domian.PersonResponse;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 @Path("/check")
 public class CheckPersonService {
     @GET
-    public String checkPerson() {
-        return "Simple String";
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PersonResponse checkPerson(@PathParam("id") int id,
+                                      @QueryParam("name") String name) {
+        return new PersonResponse();
     }
 }
