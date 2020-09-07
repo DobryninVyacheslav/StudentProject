bf6f1407c9b9d129f382a3e8083815782bb7c06f
SlavaDobr <slavad999@gmail.com>
2020-09-07 21:41:41 +0300

    refactoring

--- StudentOrder/src/main/java/edu/javacourse/studentorder/validator/register/RealCityRegisterChecker.java
+++ StudentOrder/src/main/java/edu/javacourse/studentorder/validator/register/RealCityRegisterChecker.java
@@ -1,10 +1,10 @@
 package edu.javacourse.studentorder.validator.register;
 
+import edu.javacourse.studentorder.config.Config;
+import edu.javacourse.studentorder.domain.Person;
 import edu.javacourse.studentorder.domain.register.CityRegisterRequest;
 import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
-import edu.javacourse.studentorder.domain.Person;
 import edu.javacourse.studentorder.exception.CityRegisterException;
-import edu.javacourse.studentorder.exception.TransportException;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -14,13 +14,16 @@ import javax.ws.rs.core.MediaType;
 public class RealCityRegisterChecker implements CityRegisterChecker
 {
     public CityRegisterResponse checkPerson(Person person)
-            throws CityRegisterException, TransportException {
-        CityRegisterRequest request = new CityRegisterRequest(person);
-        Client client = ClientBuilder.newClient();
-        CityRegisterResponse response = client.target("http://localhost:8080/CityRegister-1.0/rest/check")
-                .request(MediaType.APPLICATION_JSON)
-                .post(Entity.entity(request, MediaType.APPLICATION_JSON))
-                .readEntity(CityRegisterResponse.class);
-        return response;
+            throws CityRegisterException {
+        try {
+            CityRegisterRequest request = new CityRegisterRequest(person);
+            Client client = ClientBuilder.newClient();
+            return client.target(Config.getProperty(Config.CR_URL))
+                    .request(MediaType.APPLICATION_JSON)
+                    .post(Entity.entity(request, MediaType.APPLICATION_JSON))
+                    .readEntity(CityRegisterResponse.class);
+        } catch (Exception e) {
+            throw new CityRegisterException("1", e.getMessage(),e);
+        }
     }
 }
--- StudentOrder/src/test/java/edu/javacourse/studentorder/validator/register/FakeCityRegisterChecker.java
+++ StudentOrder/src/test/java/edu/javacourse/studentorder/validator/register/FakeCityRegisterChecker.java
@@ -2,10 +2,9 @@ package edu.javacourse.studentorder.validator.register;
 
 import edu.javacourse.studentorder.domain.Adult;
 import edu.javacourse.studentorder.domain.Child;
-import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
 import edu.javacourse.studentorder.domain.Person;
+import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
 import edu.javacourse.studentorder.exception.CityRegisterException;
-import edu.javacourse.studentorder.exception.TransportException;
 
 public class FakeCityRegisterChecker implements CityRegisterChecker
 {
@@ -18,8 +17,7 @@ public class FakeCityRegisterChecker implements CityRegisterChecker
     private static final String ERROR_T_1 = "1003";
     private static final String ERROR_T_2 = "2003";
 
-    public CityRegisterResponse checkPerson(Person person)
-            throws CityRegisterException, TransportException {
+    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException {
 
         CityRegisterResponse res = new CityRegisterResponse();
 
@@ -36,11 +34,6 @@ public class FakeCityRegisterChecker implements CityRegisterChecker
             if (ps.equals(ERROR_1) || ps.equals(ERROR_2)) {
                 CityRegisterException ex =
                         new CityRegisterException("1", "GRN ERROR " + ps);
-                throw ex;
-            }
-            if (ps.equals(ERROR_T_1) || ps.equals(ERROR_T_2)) {
-                TransportException ex =
-                        new TransportException("Transport ERROR " + ps);
                 throw ex;
             }
         }
