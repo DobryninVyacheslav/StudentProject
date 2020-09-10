53ea2fd15e49550f94e5f56d4e1bbb74881e493a
SlavaDobr <slavad999@gmail.com>
2020-09-10 19:45:44 +0300

    hibernate via jpa

--- ExtSystems/RegisterOffice/src/main/java/edu/javacourse/register/manager/PersonManager.java
+++ ExtSystems/RegisterOffice/src/main/java/edu/javacourse/register/manager/PersonManager.java
@@ -1,6 +1,7 @@
 package edu.javacourse.register.manager;
 
 import edu.javacourse.register.domain.Person;
+import jdk.jshell.PersistentSnippet;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
@@ -8,10 +9,38 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class PersonManager {
     public static void main(String[] args) {
+        sessionExample();
+
+        jpaExample();
+    }
+
+    private static void jpaExample() {
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("persistence");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Person newPerson = new Person();
+        newPerson.setFirstName("Алексей");
+        newPerson.setLastName("Федоров");
+        em.persist(newPerson);
+        System.out.println(newPerson.getPersonId());
+        em.getTransaction().commit();
+        em.close();
+
+        em = emf.createEntityManager();
+        List resultList = em.createQuery("FROM Person").getResultList();
+        resultList.forEach(p -> System.out.println(p));
+        em.close();
+    }
+
+    private static void sessionExample() {
         SessionFactory sf = buildSessionFactory();
         Session session = sf.openSession();
         session.getTransaction().begin();
