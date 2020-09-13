8d7c038a56af9554436eef21df9b5d06cbb9fc12
SlavaDobr <slavad999@gmail.com>
2020-09-13 21:42:50 +0300

    spring config with annotations

--- ExtSystems/RegisterOffice/src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ ExtSystems/RegisterOffice/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -6,10 +6,14 @@ import edu.javacourse.register.view.MarriageRequest;
 import edu.javacourse.register.view.MarriageResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MarriageManager {
 
     private static final Logger LOG = LoggerFactory.getLogger(MarriageManager.class);
+    @Autowired
     private MarriageDao marriageDao;
 
     public void setMarriageDao(MarriageDao marriageDao) {
--- ExtSystems/RegisterOffice/src/main/java/edu/javacourse/register/rest/MarriageController.java
+++ ExtSystems/RegisterOffice/src/main/java/edu/javacourse/register/rest/MarriageController.java
@@ -5,10 +5,14 @@ import edu.javacourse.register.view.MarriageRequest;
 import edu.javacourse.register.view.MarriageResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service("controller")
 public class MarriageController {
 
     private static final Logger LOG = LoggerFactory.getLogger(MarriageController.class);
+    @Autowired
     private MarriageManager marriageManager;
 
     public void setMarriageManager(MarriageManager marriageManager) {
