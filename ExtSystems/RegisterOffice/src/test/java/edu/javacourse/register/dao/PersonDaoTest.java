e93249053f9c85c1197614167ce2d57762bf9690
SlavaDobr <slavad999@gmail.com>
2020-09-13 19:54:43 +0300

    added certificates to db by orm

--- ExtSystems/RegisterOffice/src/test/java/edu/javacourse/register/dao/PersonDaoTest.java
+++ ExtSystems/RegisterOffice/src/test/java/edu/javacourse/register/dao/PersonDaoTest.java
@@ -1,6 +1,8 @@
 package edu.javacourse.register.dao;
 
 import edu.javacourse.register.domain.Person;
+import edu.javacourse.register.domain.PersonFemale;
+import edu.javacourse.register.domain.PersonMale;
 import org.junit.Test;
 
 import java.util.List;
@@ -13,10 +15,18 @@ public class PersonDaoTest {
     public void findPersons() {
         PersonDao dao = new PersonDao();
         List<Person> persons = dao.findPersons();
-        persons.forEach(person -> {
-            System.out.println(person.getFirstName());
-            System.out.println(person.getClass().getName());
-            System.out.println(person.getPassports().size());
+        persons.forEach(p -> {
+            System.out.println("Name:" + p.getFirstName());
+            System.out.println("Class for sex:" + p.getClass().getName());
+            System.out.println("Passports:" + p.getPassports().size());
+            System.out.println("Birth:" + p.getBirthCertificate());
+            if (p instanceof PersonMale) {
+                System.out.println("Birth Cert:" + ((PersonMale)p).getBirthCertificates().size() );
+                System.out.println("Marriage Cert:" + ((PersonMale)p).getMarriageCertificates().size() );
+            } else {
+                System.out.println("Birth Cert:" + ((PersonFemale)p).getBirthCertificates().size() );
+                System.out.println("Marriage Cert:" + ((PersonFemale)p).getMarriageCertificates().size() );
+            }
         });
     }
 }
