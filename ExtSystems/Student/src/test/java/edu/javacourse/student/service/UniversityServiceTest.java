b7eb9a1df9b7fa0ba23186081a0ce3961b07b200
SlavaDobr <slavad999@gmail.com>
2020-10-11 20:50:56 +0300

    learned LazyInitException

---
+++ ExtSystems/Student/src/test/java/edu/javacourse/student/service/UniversityServiceTest.java
@@ -0,0 +1,37 @@
+package edu.javacourse.student.service;
+
+import edu.javacourse.student.domain.Faculty;
+import edu.javacourse.student.domain.University;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = {"classpath:springContext.xml"})
+public class UniversityServiceTest {
+
+    @Autowired
+    private UniversityService service;
+
+    @Test
+    public void findUniversities() {
+        List<University> universities = service.findUniversities();
+        System.out.println("OK");
+        universities.forEach(u -> System.out.println(
+                u.getUniversityId() + ":" + u.getUniversityName() + ":" + u.getFaculties().size()));
+    }
+
+    @Test
+    public void findFaculties() {
+        List<Faculty> faculties = service.findFaculties();
+        System.out.println("OK");
+        faculties.forEach(f -> System.out.println(
+                f.getFacultyId() + ":" + f.getUniversity().getUniversityName()));
+    }
+}
